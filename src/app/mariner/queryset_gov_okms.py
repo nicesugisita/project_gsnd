8cd1842e1b7adcdbb876aa3f6d53b1277d274868
Seong Yugi <tjddbrl64@gmail.com>
2026-05-22 08:19:25 +0900

    최종응답프롬프트 및 관련성 필터 수정

--- src/app/mariner/queryset_gov_okms.py
+++ src/app/mariner/queryset_gov_okms.py
@@ -19,7 +19,6 @@ from app.core.constants import (
     OP_NOT,
     OP_HASANY,
     OP_INT_SUMMATION,
-    OP_HASANY,
 )
 from app.core.exceptions import RAGServiceError
 from app.mariner.jvm_manager import ensure_jvm_thread
